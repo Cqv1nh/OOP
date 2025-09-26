e960b5549d509f48d2417086a138a53fc1fb85b8
Cqv1nh <notoriousunknow@gmail.com>
2025-09-26 21:48:39 +0700

    constants

---
+++ Arkanoid/main/util/Constants.java
@@ -0,0 +1,26 @@
+package util
+
+import java.awt.Color;
+import java.awt.Font;
+
+public final class Constants {
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
+    public static final String SCREEN_TITLE = "Arkaniod";
+
+    public static final int PADDLE_WIDTH = 100;
+    public static final int PADDLE_HEIGHT = 20;
+
+    public static final int INIT_PADDLE_X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
+    public static final int INIT_PADDLE_Y = 50;
+
+    public static final int BALL_DIAMETER = 16;
+
+    public static final int INIT_BALL_X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
+    public static final int INIT_PADDLE_Y = 80;
+
+    public static final int BRICK_WIDTH = 50;
+    public static final int BRICK_HEIGHT = 20;
+
+    
+}
