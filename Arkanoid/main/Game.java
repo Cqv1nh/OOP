24b906ed7c893fec913bab933268c8478301bb4a
Cqv1nh <notoriousunknow@gmail.com>
2025-09-26 23:58:13 +0700

    render#2

--- Arkanoid/main/Game.java
+++ Arkanoid/main/Game.java
@@ -1,8 +1,35 @@
+import entities.Paddle;
+import renderer.RendererConsole;
+import utils.Constants;
 
-public clas Game {
+
+
+public class Game {
     public static void main(String[] args) {
-        Paddle paddle = new Paddle();
+        Paddle paddle = new Paddle(Constants.INIT_PADDLE_X,
+                Constants.INIT_PADDLE_Y, Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,0);
 
-        RendererConsole
+        RendererConsole render = new RendererConsole();
+
+        while (true) {
+            render.clearConsole();
+
+            render.DrawBorder();
+            render.DrawPaddle(paddle);
+
+            render.display();
+
+            try {
+                // Pause the program for 2000 milliseconds (2 seconds)
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                // This block is executed if another thread interrupts this one while it's sleeping.
+                // For simple games, you can often just print the stack trace.
+                e.printStackTrace();
+            }
+
+            render.CharacterClear();
+        }
     }
 }
--- Arkanoid/main/ui/RendererConsole.java
+++ Arkanoid/main/ui/RendererConsole.java
@@ -8,51 +8,53 @@ import java.util.Arrays;
 
 public class RendererConsole {
     private final int TerminalWidth = Constants.SCREEN_WIDTH / 20;
-    private final int TerminalHeight = Constants.SCREEN_HEIGHT / 10;
+    private final int TerminalHeight = Constants.SCREEN_HEIGHT / 20;
     char[][] grid;
 
     public RendererConsole() {
         this.grid = new char[TerminalHeight][TerminalWidth];
     }
 
-    private void CharacterClear() {
+    public void CharacterClear() {
         for (int i = 0; i < TerminalHeight; i++) {
-            Arrays.fill(grid[0], 0, TerminalWidth, '#');
+            Arrays.fill(grid[i], 0, TerminalWidth, ' ');
         }
     }
 
-    private void DrawBorder() {
+    public void DrawBorder() {
         Arrays.fill(grid[0], 0, TerminalWidth, '#');
         Arrays.fill(grid[TerminalHeight - 1], 0, TerminalWidth, '#');
 
-        for (int i = 0; i < TerminalHeight - 1; i++) {
+        for (int i = 0; i < TerminalHeight; i++) {
             grid[i][0] = '#';
             grid[i][TerminalWidth - 1] = '#';
         }
     }
 
-    private void DrawPaddle(Paddle paddle) {
-        int TerminalX = (int) paddle.getX() / 10;
-        int TerminalY = TerminalHeight - 5;
+    public void DrawPaddle(Paddle paddle) {
+        int TerminalX = (int) paddle.getX() / 20;
+        int TerminalY = this.TerminalHeight - 5;
+        int TerminalWidth = (int) paddle.getWidth() / 20;
 
-        int TerminalWidth = (int) paddle.getWidth() / 10;
+        for (int i = 0; i < TerminalWidth; i++) {
+            int currentX = TerminalX + i;
 
-        int endX = Math.min(TerminalX + TerminalWidth, TerminalWidth);
-        int startX = Math.max(0, TerminalX);
-
-        Arrays.fill(grid[TerminalY], startX, endX, '═');
+            if (currentX > 0 && currentX < TerminalWidth) {
+                grid[TerminalY][currentX] = '═';
+            }
+        }
     }
 
-    private void DrawBall() {
+    public void DrawBall() {
 
     }
 
-    private void DrawBrick() {
+    public void DrawBrick() {
 
     }
 
-    private void display() {
-        CharacterClear();
+    public void display() {
+
         StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < TerminalHeight; i++){
@@ -63,15 +65,17 @@ public class RendererConsole {
         System.out.print(s);
     }
 
-    private void clear(){
+    public void clearConsole() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
+                // This is a more robust way for Linux/Mac
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
             }
         } catch (Exception e) {
-            System.out.println("Error");
+            System.out.println("Error clearing console: " + e.getMessage());
         }
     }
 
