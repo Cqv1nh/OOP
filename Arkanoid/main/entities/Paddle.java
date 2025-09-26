b09ca36b6a2671d675f4e6c34df47a869657dea1
23020889-netizen <23020889@vnu.edu.vn>
2025-09-26 23:23:09 +0700

    Update Paddle.java

--- Arkanoid/main/entities/Paddle.java
+++ Arkanoid/main/entities/Paddle.java
@@ -1,14 +1,13 @@
 package main.entities;
 
-public class Paddle extends MovableObjext {
-    private double speed; // Speed la toc do, khong co huong
+public class Paddle extends MovableObject {
+    private double speed = 2.0; // Speed la toc do, khong co huong
     private String currentPowerUp;
 
     public Paddle(double x, double y, double width,
                   double height, double dx, double dy,
                   String currentPowerUp) {
         super(x, y, width, height, dx, dy);
-        this.speed = Math.abs(dx);
         this.currentPowerUp = currentPowerUp;
     }
 
@@ -19,9 +18,40 @@ public class Paddle extends MovableObjext {
         return speed;
     }
 
+    public void setCurrentPowerUp(String currentPowerUp) {
+        this.currentPowerUp = currentPowerUp;
+    }
+
+    public String getCurrentPowerUp() {
+        return currentPowerUp;
+    }
     @Override
+    public void move() { // DI chuyen den toa do X moi
+        setX(getX()+getDx());
+    }
 
+    public void moveLeft() {
+        setDx( -speed);
+        move();
+    }
 
+    public void moveRight() {
+        setDx(speed);
+        move();
+    }
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
 
-
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+        System.out.println("===");
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+    public boolean applyPowerUp() {
+        return false;
+    }
 }
