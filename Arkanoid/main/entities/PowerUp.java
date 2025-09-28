ae4fb0fa3ed5183656b7969dde1d6c2686e76a1b
trongjismeeee <23020893@vnu.edu.vn>
2025-09-28 16:50:51 +0700

    Powerup lần 2

--- Arkanoid/main/entities/PowerUp.java
+++ Arkanoid/main/entities/PowerUp.java
@@ -1,15 +1,16 @@
 package main.entities;
 
-public PowerUp extends GameObject {
+public abstract class PowerUp extends GameObject {
     private double duration; //Thời gian hiệu lực
     private String type; //Loại powerup
 
-    //Constructor không có tham số.
+    /*Constructor không có tham số.
     public PowerUp() {
-    }
+    } */
 
     //Constructor có tham số.
-    public PowerUp(String type, double duration) {
+    public PowerUp(double x, double y, double width, double height, String type, double duration) {
+        super(x, y, width, height);
         this.type = type;
         this.duration = duration;
     }
@@ -24,13 +25,27 @@ public PowerUp extends GameObject {
     }
 
     //Getter + setter cho Duration:
-    public int getDuration() {
+    public double getDuration() {
         return duration;
     }
 
-    public void setDuration() {
+    public void setDuration(double duration) {
         this.duration = duration;
     }
 
-    
+    //Áp dụng hiệu ứng:
+    public void applyEffect(Paddle paddle) {
+
+    }
+
+    //Gỡ bỏ hiệu ứng:
+    public void removeEffect(Paddle paddle) {
+
+    }
+
+    @Override
+    public abstract void update(String input); //Cập nhật vị trí theo phím bấm
+
+    @Override
+    public abstract void render(); //Vẽ đối tượng trên terminal
 }
