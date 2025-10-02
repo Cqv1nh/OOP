01b165a6a80f1d27e4f72b693bb158f5d1b28907
Cqv1nh <notoriousunknow@gmail.com>
2025-10-02 22:45:24 +0700

    fix (Game) fix bugs.

--- Arkanoid/main/Game.java
+++ Arkanoid/main/Game.java
@@ -1,34 +1,79 @@
+import engine.CollisionDetector;
+import engine.KeyboardInputTerminal;
+import entities.Ball;
+import entities.NormalBrick;
 import entities.Paddle;
 import ui.RendererConsole;
 import util.Constants;
+// import engine.KeybroadInputJPanel; // remove if not used
 
+import static util.FileLevelLoader.loadLevelFromFile;
+import engine.PaddleController;
+import util.FileLevelLoader;
 
+import java.io.IOException;
+import java.util.List;
 
 public class Game {
+    private static boolean isRunning = true;
+    public static int frameCounter = 0;
+    public void stopGame() {
+        isRunning = false;
+    }
+
     public static void main(String[] args) {
-        Paddle paddle = new Paddle(Constants.INIT_PADDLE_X,
-                Constants.INIT_PADDLE_Y, Constants.PADDLE_WIDTH,
-                Constants.PADDLE_HEIGHT,0);
+        try {
+            Paddle paddle = new Paddle(
+                    Constants.INIT_PADDLE_X,
+                    Constants.INIT_PADDLE_Y,
+                    Constants.PADDLE_WIDTH,
+                    Constants.PADDLE_HEIGHT,
+                    0, 0, "None"
+            );
 
-        RendererConsole render = new RendererConsole();
+            Ball ball = new Ball (5,5);
 
-        while (true) {
-            render.clearConsole();
+            RendererConsole render = new RendererConsole();
 
-            render.DrawBorder();
-            render.DrawPaddle(paddle);
 
-            render.display();
+            //List<NormalBrick> bricks = FileLevelLoader.loadLevelFromFile("resources/level1.txt");
 
-            try {
+            KeyboardInputTerminal kit = new KeyboardInputTerminal();
+            PaddleController pc = new PaddleController(kit);
 
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-            }
-
+            kit.start(); // start listening for key presses
             render.CharacterClear();
+
+            while (isRunning) {
+                render.clearConsole();
+
+                if (!pc.movePaddle(paddle)) {
+                    isRunning = false;
+                }
+
+                ball.move();
+                CollisionDetector.checkWallCollision(ball);
+
+                render.DrawBorder();
+                render.DrawPaddle(paddle);
+                render.DrawBall(ball);
+                //render.DrawNormalBrick(bricks);
+
+                render.display();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                render.CharacterClear();
+                frameCounter++;
+            }
+            kit.stop(); // stop input thread safely when game ends
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        System.out.println(frameCounter);
     }
 }
