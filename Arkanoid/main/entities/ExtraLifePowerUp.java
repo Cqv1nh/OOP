3046700f77c5d6ff5050ae554ee5c9489a29182b
trongjismeeee <23020893@vnu.edu.vn>
2025-10-06 22:09:47 +0700

    feat: add ExtraLifePowerUp class for increase PowerUp effect (1st)

---
+++ Arkanoid/main/entities/ExtraLifePowerUp.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ExtraLifePowerUp extends PowerUp {
+    public ExtraLifePowerUp(double x, double y, double width, double height, double duration) {
+        super(x, y, width, height, "EXTRA_LIFE", duration);
+    }
+
+    //Áp dụng hiệu ứng thêm 1 mạng.
+    @Override
+    public void applyEffect(Paddle paddle) {
+        /* **CHÚ Ý**: Cần thêm mạng vào GameManager
+         * Thêm: gameManager.addLife();
+         * Hoặc: lives++;
+         * Hoặc: setLives(getLives() + 1);
+         */
+    }
+
+    //Xóa bỏ hiệu ứng.
+    @Override
+    public void removeEffect(Paddle paddle) {
+    } //Hàm này k làm gì cả vì ng chơi mất mạng khi không còn bóng.
+
+    //Cập nhật vị trí đối tượng.
+    @Override
+    public void update(String input) {
+        setX(getX());
+        setY(getY() + 1); //tăng y lên 1 do đi xuống.
+    }
+
+    @Override
+    public void render() {
+        // Vẽ ký tự '*' tại vị trí (x, y) theo ANSI escape code
+        System.out.print("\033[" + ((int)getY() + 1) + ";" + ((int)getX() + 1) + "H*");
+    }
+}
+
