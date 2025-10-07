488fef0d9222958a4b4156d938bfad3b78211da0
23020889-netizen <23020889@vnu.edu.vn>
2025-10-07 23:48:26 +0700

    Update BrickManager.java
    Căn chỉnh gạch cho đều 2 bên màn hình

--- Arkanoid/main/ui/BrickManager.java
+++ Arkanoid/main/ui/BrickManager.java
@@ -10,7 +10,7 @@ public class BrickManager {
         ArrayList<Brick> brickList = new ArrayList<>();
         // Dat cac vien gach cach nhau
         for (int i = 60; i <= 120; i= i + Constants.BRICK_HEIGHT + 5) {
-            for(int j = 60; j <= 680; j = j + Constants.BRICK_WIDTH + 5) {
+            for(int j = 55; j <= 680; j = j + Constants.BRICK_WIDTH + 5) {
                 brickList.add(new NormalBrick(j, i, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
             }
         }
