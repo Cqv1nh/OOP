6a7341cee2af5d8c41ee2245b0b4e2bccc231747
23020889-netizen <23020889@vnu.edu.vn>
2025-10-13 09:49:17 +0700

    feat(level): Implement data-driven brick creation via 2D layout
    Refactored BrickManager to use a 2D array (LEVEL_1_LAYOUT) for defining the game level structure.
    
    This change replaces the old hardcoded brick generation loops with a dynamic system that maps brick codes to specific brick types (Normal, Strong, Explosive, Unbreakable) and automatically calculates their positions based on the grid layout.

--- Arkanoid/main/ui/BrickManager.java
+++ Arkanoid/main/ui/BrickManager.java
@@ -1,26 +1,61 @@
 // package ui;
 
-import entities.Brick;
-import entities.NormalBrick;
-import entities.StrongBrick;
+import entities.*;
 import util.Constants;
 
 import java.util.ArrayList;
 
 // Tao class tao cac brick va quan ly chung, dc goi trong GameWindow
 public class BrickManager {
+
+    //Thuoc tinh dinh nghia cau truc gach bang 1 mang 2D 
+    // 0: NULL, 1: NormalBrick 2. StrongBrick, 3. ExplosiveBrick, 4.UnbreakableBrick
+    private static final int[][] LEVEL_1_LAYOUT = {
+        {0, 1, 1, 1, 1, 4, 4, 1, 1, 1, 0},
+        {1, 1, 2, 2, 2, 4, 4, 2, 2, 1, 1},
+        {1, 2 ,1 ,1, 3, 1, 3, 1, 1, 2, 1}
+    };
+
     public ArrayList<Brick> createBricks() {
         ArrayList<Brick> brickList = new ArrayList<>();
+        // Lay kich thuoc mang dong va cot
+        int numRows = LEVEL_1_LAYOUT.length;
+        int numCols = LEVEL_1_LAYOUT[0].length;
+        
+        // Can le cho toan bo khoi gach moi vien gach cach nhau 5 theo truc x
+        double totalBricksWidth = numCols * Constants.BRICK_WIDTH + ((numCols) - 1) * 5;
+        double startX = (Constants.SCREEN_WIDTH - totalBricksWidth) / 2;
+        double startY = 60; // Vi tri bat dau theo truc y
+
         // Dat cac vien gach cach nhau
-        for (int i = 60; i <= 85; i = i + Constants.BRICK_HEIGHT + 5) {
-            for (int j = 55; j <= 705; j = j + Constants.BRICK_WIDTH + 5) {
-                brickList.add(new NormalBrick(j, i, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
+        for (int i= 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                int brickCode = LEVEL_1_LAYOUT[i][j];
+                // Neu trong thi bo qua
+                if (brickCode == 0) {
+                    continue;
+                }
+                double x = startX + j * (Constants.BRICK_WIDTH + 5);
+                double y = startY + i * (Constants.BRICK_HEIGHT + 5);
+                
+                switch (brickCode) {
+                    case 1:
+                        brickList.add(new NormalBrick(x, y, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
+                        break;
+                    case 2:
+                        brickList.add(new StrongBrick(x, y, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
+                        break;
+                    case 3:
+                        brickList.add(new ExplosiveBrick(x, y, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
+                        break;
+                    case 4:
+                        brickList.add(new UnbreakableBrick(x, y, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        for (int j = 55; j <= 705; j = j + Constants.BRICK_WIDTH + 5) {
-                brickList.add(new StrongBrick(j, 110, Constants.BRICK_WIDTH, Constants.BRICK_HEIGHT));
-        }    
-        
         return brickList;
     }
 }
